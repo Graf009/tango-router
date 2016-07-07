d623aee028a42e9b5a97284da9861677a019f3d1
Oleg Orlov <mail@orlovoleg.com>
2016-07-07 16:57:01 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,6 @@ var _extends2 = require('babel-runtime/helpers/extends');
 
 var _extends3 = _interopRequireDefault(_extends2);
 
-var _reducers, _actions;
-
 var _reactRouterRedux = require('react-router-redux');
 
 var _reactRouter = require('react-router');
@@ -31,19 +29,31 @@ var builtins = {
   Auth: _Auth2.default
 };
 
-var middleware = (0, _reactRouterRedux.syncHistory)(_reactRouter.browserHistory);
+var history = {
+  browserHistory: _reactRouter.browserHistory,
+  createMemoryHistory: _reactRouter.createMemoryHistory
+};
+
 var getRouterState = function getRouterState(state) {
   return state.get(moduleName).location;
 };
+var createPlugin = function createPlugin(history) {
+  var _reducers, _actions;
 
-exports.default = (0, _extends3.default)({}, builtins, {
-  history: _reactRouter.browserHistory,
-  reducers: (_reducers = {}, _reducers[moduleName] = _reactRouterRedux.routeReducer, _reducers),
-  actions: (_actions = {}, _actions[moduleName] = _reactRouterRedux.routeActions, _actions),
-  middleware: middleware,
-  hook: function hook(store) {
-    middleware.listenForReplays(store, getRouterState);
-    return store;
-  }
+  var middleware = (0, _reactRouterRedux.syncHistory)(history);
+
+  return {
+    reducers: (_reducers = {}, _reducers[moduleName] = _reactRouterRedux.routeReducer, _reducers),
+    actions: (_actions = {}, _actions[moduleName] = _reactRouterRedux.routeActions, _actions),
+    middleware: middleware,
+    hook: function hook(store) {
+      middleware.listenForReplays(store, getRouterState);
+      return store;
+    }
+  };
+};
+
+exports.default = (0, _extends3.default)({}, builtins, history, {
+  createPlugin: createPlugin
 });
 module.exports = exports['default'];
--- es/index.js
+++ es/index.js
@@ -1,9 +1,6 @@
 import _extends from 'babel-runtime/helpers/extends';
-
-var _reducers, _actions;
-
 import { routeActions, syncHistory, routeReducer } from 'react-router-redux';
-import { browserHistory, Router, Route, Link, Redirect, IndexRoute, IndexLink, IndexRedirect } from 'react-router';
+import { browserHistory, createMemoryHistory, Router, Route, Link, Redirect, IndexRoute, IndexLink, IndexRedirect } from 'react-router';
 
 import Auth from './lib/Auth';
 
@@ -20,18 +17,30 @@ var builtins = {
   Auth: Auth
 };
 
-var middleware = syncHistory(browserHistory);
+var history = {
+  browserHistory: browserHistory,
+  createMemoryHistory: createMemoryHistory
+};
+
 var getRouterState = function getRouterState(state) {
   return state.get(moduleName).location;
 };
+var createPlugin = function createPlugin(history) {
+  var _reducers, _actions;
 
-export default _extends({}, builtins, {
-  history: browserHistory,
-  reducers: (_reducers = {}, _reducers[moduleName] = routeReducer, _reducers),
-  actions: (_actions = {}, _actions[moduleName] = routeActions, _actions),
-  middleware: middleware,
-  hook: function hook(store) {
-    middleware.listenForReplays(store, getRouterState);
-    return store;
-  }
+  var middleware = syncHistory(history);
+
+  return {
+    reducers: (_reducers = {}, _reducers[moduleName] = routeReducer, _reducers),
+    actions: (_actions = {}, _actions[moduleName] = routeActions, _actions),
+    middleware: middleware,
+    hook: function hook(store) {
+      middleware.listenForReplays(store, getRouterState);
+      return store;
+    }
+  };
+};
+
+export default _extends({}, builtins, history, {
+  createPlugin: createPlugin
 });
